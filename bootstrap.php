ebaff28e909f1a457d75e4bf1b250ccf1129603b
Matt Ryan <matt@capwebsolutions.com>
2018-04-07 19:17:30 -0400

    initial commit of code

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name
+ *
+ * @package     PluginPackage
+ * @author      Your Name
+ * @copyright   2016 Your Name or Company Name
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Collapsible Content
+ * Plugin URI: https://github.com/CApWebSolutions/
+ * Description: Collapsible Content is a WordPress plugin that shows and hdes content. Practical examples include Q&As, FAQs, hints, and more. Click the icon to open the content. Click it again to hide the content.  
+ * Version: 1.0.0
+ * Author: Cap Web Solutions
+ * Author URI: https://capwebsolutions.com
+ * Text Domain: collapsible_content
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
